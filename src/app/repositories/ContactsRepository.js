f82e936b4889f07a57d8e0efac50bfee914b7538
Iana Sampaio <iana.sampaio06@gmail.com>
2023-12-13 12:01:21 -0300

    update contact controller

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -31,12 +31,49 @@ class ContactsRepository {
     })
   }
 
+  findByEmail(email){
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email ===email));
+    })
+  }
+
   delete(id) {
     return new Promise((resolve) => {
-      setTimeout(() => {
-        contacts = contacts.filter((contact) => contact.id !== id);
-        resolve();
-      }, 1000); // Atraso de 1 segundo (1000 milissegundos) como exemplo
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+
+  create(name, email, phone, category_id) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {name, email, phone, category_id}) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updatedContact : contact
+      ));
+
+      resolve(updatedContact);
     });
   }
 
