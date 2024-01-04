d4a3a05f4bb90851b9eeb27aa24b09e29988aec4
Iana Sampaio <iana.sampaio06@gmail.com>
2024-01-04 10:28:33 -0300

    update controllers and database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./router');
+require('express-async-errors');
 
 const app = express();
-
-app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000,() => console.log('Server started at http://localhost:3000'));
